571133efe110ad3cd74945919f7c84dafc50bc6c
Arulselvan Madhavan <arulselvan1234@gmail.com>
2024-09-12 23:32:57 -0700

    ccl init

---
+++ bin/ccl.ml
@@ -0,0 +1,49 @@
+open Base
+open Link
+module Sim = Toposim.Simulator
+module Signal = Sim.Signal
+module Process = Sim.Process
+module Async = Sim.Async
+
+let reduce_scatter n conn payload dst_mat link_mat =
+  let open Sim in
+  let open Link_signal.Link_value in
+  match conn with
+  | `All2All ->
+    (* Send to all uplinks *)
+    let trigger_send xpu_id =
+      let payload = payload / n in
+      let send_time = payload / 1 in
+      let uls = link_mat.(xpu_id) in
+      let ul_ids = Array.map uls ~f:(Signal.id) in
+      let handle_sends () =
+        let handle_send ul =
+          match !!ul.status with
+          | Ready ->
+            let delay = get_delay !!ul.status in
+            let lv = {!!ul with status = Sending send_time; update_time = Async.current_time ()} in
+            (ul <--- lv) ~delay
+          | _ -> ()
+        in
+      Array.iter uls ~f:handle_send
+      in
+      Process.create (Array.to_list ul_ids) handle_sends
+    in
+    let send_procs = Array.init n ~f:(trigger_send) in
+    let trigger_recv xpu_id =
+      let dls = Link.downlinks_for_xpu xpu_id dst_mat link_mat in
+      let dl_ids = Array.map dls ~f:(Signal.id) in
+      let handle_recvs () =
+        let handle_recv dl =
+          match !!dl.status with
+          | Sending delay ->
+            let lv = {!!dl with status = Complete; update_time = Async.current_time ()} in
+            (dl <--- lv) ~delay
+          | _ -> ()
+        in
+        Array.iter dls ~f:handle_recv
+      in
+      Process.create (Array.to_list dl_ids) handle_recvs
+    in
+    let recv_procs = Array.init n ~f:(trigger_recv) in
+    List.map [send_procs; recv_procs] ~f:(Array.to_list) |> List.concat
--- bin/main.ml
+++ bin/main.ml
@@ -4,60 +4,7 @@ module Signal = Sim.Signal
 module Process = Sim.Process
 module Async = Sim.Async
 module Debug = Sim.Debug
-
-module Link_signal = struct
-  module Link_value = struct
-    type link_status =
-      | Undefined
-      | Connecting
-      | Ready
-      | InUse
-    [@@deriving equal, sexp_of, compare]
-
-    type t =
-      { src : int
-      ; dst : int
-      ; status : link_status
-      ; update_time : int
-      }
-    [@@deriving sexp_of, equal, compare, fields ~getters]
-
-    let undefined = { src = -1; dst = -1; status = Undefined; update_time = -1 }
-
-    let ( = ) l1 l2 =
-      let { src = l1_src; dst = l1_dst; status = l1_st; update_time = l1_ut } = l1 in
-      let { src = l2_src; dst = l2_dst; status = l2_st; update_time = l2_ut } = l2 in
-      Int.(l1_src = l2_src)
-      && Int.(l1_dst = l2_dst)
-      && equal_link_status l1_st l2_st
-      && Int.(l1_ut = l2_ut)
-    ;;
-
-    let resolve_value =
-      `Func
-        (fun ~last_value xs ->
-          let xs = Core.List.stable_dedup xs ~compare in
-          match xs with
-          | [] -> last_value
-          | [ x ] -> x
-          | xs ->
-            (* If multiple processes are writing, pick the latest write *)
-            Core.List.sort xs ~compare:(fun a b ->
-              Int.compare (update_time b) (update_time a))
-            |> List.hd_exn)
-    ;;
-
-    let check_value_compatibility _ = ()
-    let initial_value = undefined
-
-    let get_delay = function
-      | Undefined | Connecting | Ready -> 1
-      | InUse -> 0
-    ;;
-
-    let make_t src dst status time = { src; dst; status; update_time = time }
-  end
-end
+open Link
 
 let make_links dsts =
   Array.init (Array.length dsts) ~f:(fun _ ->
@@ -71,7 +18,7 @@ let make_xpu link_mat dst_mat xpu_id =
   (* downlink processes - listen to changes from processes for which it's the dst *)
   let open Sim in
   let open Link_signal.Link_value in
-  let _ = InUse in
+  (* Uplink Process *)
   let uls = link_mat.(xpu_id) in
   let ul_ids = Array.map uls ~f:(Signal.id) in
   let dsts = dst_mat.(xpu_id) in
@@ -88,14 +35,8 @@ let make_xpu link_mat dst_mat xpu_id =
     Array.iteri uls ~f:establish_conn
   in
   let ul_proc = Process.create (Array.to_list ul_ids) (handle_ul) in
-  let dls = Array.map dsts ~f:(fun d ->
-      let dsts_from_d = dst_mat.(d) in
-      let uls_from_d = link_mat.(d) in
-      let dlinks = Array.zip_exn dsts_from_d uls_from_d in
-      let (_, dlink) = Array.find_exn dlinks ~f:(fun (d, _) -> Int.(d = xpu_id)) in
-      dlink
-    )
-  in
+  (* Downlink process  *)
+  let dls = Link.downlinks_for_xpu xpu_id dst_mat link_mat in
   let dl_ids = Array.map dls ~f:(Signal.id) in
   let handle_dls () =
     let handle_dl dl =
@@ -134,7 +75,8 @@ let () =
     Array.fold dbg_mat ~init:[] ~f:(Fn.flip List.cons) |> Array.concat |> Array.to_list
   in
   let xpus = Array.fold xpus ~init:[] ~f:(Fn.flip List.cons) |> List.concat in
-  let xpus = xpus @ dbgs in
+  let comms = Ccl.reduce_scatter num_xpus `All2All (num_xpus * 100) dst_mat link_mat in
+  let xpus = xpus @ comms @ dbgs in
   let xpusim = Sim.create xpus in
-  Sim.run xpusim ~time_limit:8
+  Sim.run xpusim ~time_limit:200
 ;;
