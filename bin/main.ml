f0d52cea28e611218970d1c284be950c64bd80da
Arulselvan Madhavan <arulselvan1234@gmail.com>
2024-10-03 16:26:46 -0400

    Term to switch send works

--- bin/main.ml
+++ bin/main.ml
@@ -9,28 +9,7 @@ module Ccl = Toposim.Ccl
 module Conn_type = Toposim.Conn_type
 module LT = Toposim.Link_type
 open Link
-
-let pf = Stdio.printf
-let _shape_str mat = pf "%d,%d\n" (Array.length mat) (Array.length mat.(0))
-
-let flatten_mat mat =
-  let dimx = Array.length mat in
-  let dimy = Array.length mat.(0) in
-  Array.init (dimx * dimy) ~f:(fun i ->
-    let row_id = i / dimy in
-    let col_id = i - (row_id * dimy) in
-    (* pf "flatten:%d|%d\n" row_id col_id; *)
-    mat.(row_id).(col_id))
-;;
-
-let transpose mat =
-  let dimy = Array.length mat in
-  assert (dimy > 0);
-  let dimx = Array.length mat.(0) in
-  let tmat = Array.make_matrix ~dimx ~dimy mat.(0).(0) in
-  Array.iteri mat ~f:(fun i row -> Array.iteri row ~f:(fun j e -> tmat.(j).(i) <- e));
-  tmat
-;;
+open Toposim.Utils
 
 let make_links dst_mat =
   Array.map dst_mat ~f:(fun dsts ->
